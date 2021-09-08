8d61caed156f340f31e56799848205e1dbedc0ae
Michaela Raines <kellar.dev@gmail.com>
2021-09-08 10:11:47 -0400

    deleted comments from post class, postservice

--- 24Hr.Data/Post.cs
+++ 24Hr.Data/Post.cs
@@ -17,14 +17,7 @@ namespace _24Hr.Data
         public string Text { get; set; }
         [Required]
         public Guid AuthorId { get; set; }
-        //public virtual List<Comment> Comments { get; set; } = new List<Comment>();
-        /*public int NumberOfComments
-        {
-            get
-            {
-                return Comments.Count;
-            }
-        }*/
+        public virtual List<Comment> Comments { get; set; } = new List<Comment>();
         
         [Required]
         public DateTimeOffset CreatedUtc { get; set; }
